d756af81e8a89c4f9c1cb62fab88596404a0547f
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-05 12:58:29 -0500

    hardness for wordlist

--- src/main/java/HarnessForWordList.java
+++ src/main/java/HarnessForWordList.java
@@ -1,2 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HarnessForWordList {
+
+    private DashaMapOne dashaMapOne;
+    private DashaMapTwo dashaMapTwo;
+    private DashaMapThree dashaMapThree;
+
+    public void readFile(){
+        dashaMapOne = new DashaMapOne();
+        dashaMapTwo = new DashaMapTwo();
+        dashaMapThree = new DashaMapThree();
+
+        String filePath = "word-list.txt";
+        String line;
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            while ((line = reader.readLine()) != null){
+                String[] words = line.split(" ", 2);
+                if(words.length >= 2){
+                    String key = words[0];
+                    String value = words[1];
+                    dashaMapOne.set(key, value);
+                    dashaMapTwo.set(key, value);
+                    dashaMapThree.set(key, value);
+                }
+            }
+            reader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public DashaMapOne getDashaMapOne(){
+        return dashaMapOne;
+    }
+
+    public DashaMapTwo getDashaMapTwo(){
+        return dashaMapTwo;
+    }
+
+    public DashaMapThree getDashaMapThree(){
+        return dashaMapThree;
+    }
 }
